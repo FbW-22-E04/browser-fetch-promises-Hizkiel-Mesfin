b23a2f09e2af182a0af3391e01cedb4ba6d79dd7
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-09-28 15:38:11 +0200

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,23 @@
-// The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
-// Import any additional modules you want to include below \/
-
+// // The following line makes sure your styles are included in the project. Don't remove this.
+// import "../styles/main.scss";
+// // Import any additional modules you want to include below \/
 
 // \/ All of your javascript should go here \/
 
+const modal = document.querySelector("#myModal");
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 60000);
+});
+
+myPromise.then(() => {
+  modal.style.display = "block";
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList.contains("close")) {
+    modal.style.display = "none";
+  }
+});
